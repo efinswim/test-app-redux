7186f401f68dad0b037c71d28ec1d6dc48fc6d6e
Andrii Rastorhuiev <efinswim@gmail.com>
2022-05-02 16:56:15 +0300

    initial project, added components likes, connect to store, logic likes component

---
+++ src/redux/store.js
@@ -0,0 +1,25 @@
+import { legacy_createStore as createStore} from 'redux';
+
+const initialState = {
+  likes: 10
+}
+
+const reducer = (state = initialState, action) => {
+  console.log('reducer:', action);
+
+  switch (action.type) {
+    case 'INCREMENT':
+      return {
+        ...state,
+        likes: state.likes + 1
+      }
+
+    default:
+      return state; 
+  }
+
+}
+
+const store = createStore(reducer)
+
+export default store;
